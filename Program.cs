c516bb6d262981a50d8e0c6c176887986ba081fe
Beam <pakapon.krin@mail.kmutt.ac.th>
2023-03-13 16:09:41 +0700

    add1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace midterm {
+    class Program {
+        static void Main(string[] args){
+            
+            Console.Write("input x1 : ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("input y1 : ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.Write("input w1 : ");
+            double w1 = double.Parse(Console.ReadLine());
+            Console.Write("input h1 : ");
+            double h1 = double.Parse(Console.ReadLine());
+
+            Console.Write("input x2 : ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("input y2 : ");
+            double y2 = double.Parse(Console.ReadLine());
+            Console.Write("input w2 : ");
+            double w2 = double.Parse(Console.ReadLine());
+            Console.Write("input h2 : ");
+            double h2 = double.Parse(Console.ReadLine());
+            
+            double a1 = w1 * h1 * 4;
+            double a2 = w2 * h2 * 4;
+            
+            if (a1 - a2 > 8  ) {
+                Console.WriteLine("Too Much Overlaping");
+            } else if (a1 - a2 < 8) {
+                Console.WriteLine("Not Much Overlapping");
+            } else {
+                Console.WriteLine("No Overlapping");
+            }
+        }
+    }
+}    
